3121e21d8e529f4f96fc1113c857de72aeefb99b
zellko <wyttenda@tutanota.com>
2022-08-25 20:50:25 +0200

    Added onClick on gameTarget.js buttons

--- src/components/Game/GameTarget/GameTarget.js
+++ src/components/Game/GameTarget/GameTarget.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './GameTarget.css';
 
-function GameTarget({ position }) {
+function GameTarget({ position, onGameTargetButtonClick }) {
   // console.log(position);
   return (
     <div
@@ -16,8 +16,8 @@ function GameTarget({ position }) {
     >
       <div className="target" />
       <div className="gameTargetButton">
-        <button type="button">Waldo</button>
-        <button type="button">Odlaw</button>
+        <button type="button" onClick={onGameTargetButtonClick}>Waldo</button>
+        <button type="button" onClick={onGameTargetButtonClick}>Odlaw</button>
       </div>
     </div>
   );
