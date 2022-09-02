6d0848a548efc9fe6292945162db673ab3ab31bb
zellko <wyttenda@tutanota.com>
2022-09-02 18:16:32 +0200

    Created Home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import waldoImg from '../../img/waldo_portrait.png';
+import odlawImg from '../../img/odlaw_portrait.png';
+import './Home.css';
 
-function Game() {
+function Home() {
   return (
     <div className="home">
-      Home
+      <h2>Your goal is to find Waldo and Odlaw!</h2>
+      <div className="home-img">
+        <img src={waldoImg} alt="Waldo portrait" />
+        <img src={odlawImg} alt="Ocdlaw portrait" />
+      </div>
+      <h3>You will be timed. Click Play game when you are ready</h3>
+      <button type="button">
+        {' '}
+        <Link to="/game">Play Game</Link>
+      </button>
     </div>
   );
 }
 
-export default Game;
+export default Home;
