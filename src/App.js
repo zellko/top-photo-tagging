966b7a2baf4064f383cb838e6b04600dbd3d09d4
zellko <wyttenda@tutanota.com>
2022-09-02 20:05:59 +0200

    Added basename

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename="/">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/game" element={<Game />} />
